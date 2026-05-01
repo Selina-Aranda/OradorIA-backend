953c72d3b5b5cbbdf4397e0dbe607757f62b4210
Selina Aranda <selinacristal043@gmail.com>
2026-05-01 00:50:09 -0500

    first commit

---
+++ src/main/java/com/utp/DemoOratorIA/DemoOratorIaApplication.java
@@ -0,0 +1,13 @@
+package com.utp.DemoOratorIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOratorIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOratorIaApplication.class, args);
+	}
+
+}
