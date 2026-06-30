aef6fed25d1e3ea2115619990b966d39c76de5cb
Roger <U22312186@utp.edu.pe>
2026-06-30 01:16:07 -0500

    Cambios planes/pago

--- src/main/java/com/utp/DemoOratorIA/infraestructure/controller/MainUserController.java
+++ src/main/java/com/utp/DemoOratorIA/infraestructure/controller/MainUserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.utp.DemoOratorIA.application.service.UserService;
 
@@ -62,5 +63,16 @@ public class MainUserController {
     public String mostrarUsuarios(Model model) {
         model.addAttribute("usuarios", userService.listar());
         return "admin-users";
+    }
+
+    @GetMapping("/planes")
+    public String mostrarPlanes() {
+        return "planes";
+    }
+
+    @GetMapping("/pago")
+    public String mostrarPago(@RequestParam String plan, Model model) {
+        model.addAttribute("plan", plan);
+        return "pago";
     }
 }
